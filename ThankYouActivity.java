b6a670493b805b09bdd58eb4fc93729d8cb4194f
Swarup Dey Sarkar <deysarkarswarup123@gmail.com>
2020-08-21 23:46:19 +0530

    Add files via upload

---
+++ ThankYouActivity.java
@@ -0,0 +1,40 @@
+package com.example.artisanprofilingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ThankYouActivity extends AppCompatActivity {
+    Button finish,newentry;
+    SharedPreferences myPref;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thank_you);
+        finish = (Button) findViewById(R.id.finish);
+        newentry = (Button) findViewById(R.id.newEntry);
+        myPref = getApplicationContext().getSharedPreferences("MyPref",MODE_PRIVATE);
+        myPref.edit().putString("track","17").apply();
+        finish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                ThankYouActivity.this.finish();
+//                System.exit(0);
+                finishAffinity();
+                System.exit(0);
+            }
+        });
+
+        newentry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(ThankYouActivity.this,MainActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
