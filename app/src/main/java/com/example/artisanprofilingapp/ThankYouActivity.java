e294131d2531f1a3d55b070188eecef853ce7e8f
Swarup Dey Sarkar <deysarkarswarup123@gmail.com>
2020-08-22 00:15:43 +0530

    Add files via upload

--- app/src/main/java/com/example/artisanprofilingapp/ThankYouActivity.java
+++ app/src/main/java/com/example/artisanprofilingapp/ThankYouActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,6 +12,7 @@ import android.widget.Button;
 public class ThankYouActivity extends AppCompatActivity {
     Button finish,newentry;
     SharedPreferences myPref;
+    private MediaPlayer mediaPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +20,8 @@ public class ThankYouActivity extends AppCompatActivity {
         finish = (Button) findViewById(R.id.finish);
         newentry = (Button) findViewById(R.id.newEntry);
         myPref = getApplicationContext().getSharedPreferences("MyPref",MODE_PRIVATE);
+        mediaPlayer = MediaPlayer.create(this, R.raw.thankyouinst);
+        mediaPlayer.start();
         myPref.edit().putString("track","17").apply();
         finish.setOnClickListener(new View.OnClickListener() {
             @Override
