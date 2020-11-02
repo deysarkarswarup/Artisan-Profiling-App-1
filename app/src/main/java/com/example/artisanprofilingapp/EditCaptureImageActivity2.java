82e30e38bf8f8b6e23c9b2eded1252114c499a72
Swarup Dey Sarkar <deysarkarswarup123@gmail.com>
2020-11-03 00:32:48 +0530

    adding update image capture intents

---
+++ app/src/main/java/com/example/artisanprofilingapp/EditCaptureImageActivity2.java
@@ -0,0 +1,14 @@
+package com.example.artisanprofilingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditCaptureImageActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_capture_image2);
+    }
+}
---
+++ app/src/main/java/com/example/artisanprofilingapp/EditCaptureImageActivity3.java
@@ -0,0 +1,14 @@
+package com.example.artisanprofilingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditCaptureImageActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_capture_image3);
+    }
+}
