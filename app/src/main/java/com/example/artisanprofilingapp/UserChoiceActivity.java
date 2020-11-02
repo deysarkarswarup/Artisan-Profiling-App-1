24f286a6092b6cd24904acd088ff52b74e83a295
Swarup Dey Sarkar <deysarkarswarup123@gmail.com>
2020-11-02 21:22:17 +0530

    fixing bugs

--- app/src/main/java/com/example/artisanprofilingapp/UserChoiceActivity.java
+++ app/src/main/java/com/example/artisanprofilingapp/UserChoiceActivity.java
@@ -35,7 +35,7 @@ public class UserChoiceActivity extends AppCompatActivity {
                 @Override
                 public void onClick(View view) {
                     mediaPlayer.stop();
-                    Intent i = new Intent(UserChoiceActivity.this, CaptureImageActivity.class);
+                    Intent i = new Intent(UserChoiceActivity.this, ImageCaptureSelection.class);
                     startActivity(i);
                 }
             });
